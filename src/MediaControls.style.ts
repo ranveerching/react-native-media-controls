14b06d2710fd46b86554fe150c609a64995b400d
Pankaj Sood <32381705+ranveerching@users.noreply.github.com>
2021-08-20 13:48:14 +0530

    Update MediaControls.style.ts

--- src/MediaControls.style.ts
+++ src/MediaControls.style.ts
@@ -7,7 +7,7 @@ const white = "#fff";
 export default StyleSheet.create({
   container: {
     alignItems: "center",
-    backgroundColor: 'lightgreen',
+    backgroundColor: containerBackgroundColor,
     bottom: 0,
     flex: 1,
     flexDirection: "column",
