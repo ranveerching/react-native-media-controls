402961efcd24ed6ddcb1752caf7331c0d91e8ba0
Pankaj Sood <32381705+ranveerching@users.noreply.github.com>
2021-08-20 12:42:50 +0530

    Update Slider.tsx

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -68,32 +68,36 @@ const Slider = (props: Props) => {
       style={[styles.controlsRow, styles.progressContainer, containerStyle]}
     >
       <View style={styles.progressColumnContainer}>
-        {!isDisabled && (
-          <>
-            <View style={[styles.timerLabelsContainer]}>
-              <Text style={styles.timerLabel}>
-                {humanizeVideoDuration(progress)}
-              </Text>
-              <Text style={styles.timerLabel}>
-                {humanizeVideoDuration(duration)}
-              </Text>
-            </View>        
-            <RNSlider
-              style={[styles.progressSlider]}
-              onValueChange={dragging}
-              onSlidingComplete={seekVideo}
-              maximumValue={Math.floor(duration)}
-              value={Math.floor(progress)}
-              trackStyle={[styles.track, customTrackStyle]}
-              thumbStyle={[
-                styles.thumb,
-                customThumbStyle,
-                { borderColor: isDisabled ? disabledColor : mainColor },
-              ]}
-              minimumTrackTintColor={isDisabled ? disabledColor : mainColor}
-            />
-          </>
-        )}        
+        {(() => {
+          if (!isDisabled) {
+            return (
+              <>
+                <View style={[styles.timerLabelsContainer]}>
+                  <Text style={styles.timerLabel}>
+                    {humanizeVideoDuration(progress)}
+                  </Text>
+                  <Text style={styles.timerLabel}>
+                    {humanizeVideoDuration(duration)}
+                  </Text>
+                </View>        
+                <RNSlider
+                  style={[styles.progressSlider]}
+                  onValueChange={dragging}
+                  onSlidingComplete={seekVideo}
+                  maximumValue={Math.floor(duration)}
+                  value={Math.floor(progress)}
+                  trackStyle={[styles.track, customTrackStyle]}
+                  thumbStyle={[
+                    styles.thumb,
+                    customThumbStyle,
+                    { borderColor: isDisabled ? disabledColor : mainColor },
+                  ]}
+                  minimumTrackTintColor={isDisabled ? disabledColor : mainColor}
+                />
+              </>
+            );
+          }
+        })()}  
       </View>
       {Boolean(onFullScreen) && (
         <TouchableOpacity
