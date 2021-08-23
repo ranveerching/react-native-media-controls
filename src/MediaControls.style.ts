abfbfb9f330b5252414fe3e94aa7c51d798b413d
Pankaj Sood <32381705+ranveerching@users.noreply.github.com>
2021-08-23 13:24:08 +0530

    Update MediaControls.style.ts

--- src/MediaControls.style.ts
+++ src/MediaControls.style.ts
@@ -6,7 +6,11 @@ const white = "#fff";
 
 export default StyleSheet.create({
   button: {
-    flex: 1,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    left: 0,
+    bottom: 0,
     backgroundColor: 'lightgreen',
   },
   container: {
